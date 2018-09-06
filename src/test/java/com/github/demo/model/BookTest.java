a86548a3e4f4a6f419edfd011ef0518abd9838e3
Yuichi Tanaka <yuichielectric@github.com>
2018-09-06 14:29:16 +0900

    テストケースを追加

--- src/test/java/com/github/demo/model/BookTest.java
+++ src/test/java/com/github/demo/model/BookTest.java
@@ -26,6 +26,12 @@ public class BookTest {
     }
 
     @Test
+    public void testConstructorWithRating() {
+        Book book = new Book("Kurt Vonnegut", "Slapstick", "scum.png", 5);
+        Assert.assertEquals(5, book.getRating().intValue());
+    }
+
+    @Test
     public void testGetCover() {
         book.setCover("slapstick.jpg");
         Assert.assertEquals("slapstick.jpg", book.getCover());
